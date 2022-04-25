5bde9dfc37d2255034a6fe47960eec343bdc3f7a
Tseng Tofu <tsengbatty@gmail.com>
2022-04-25 22:17:10 +0800

    第二週主線任務開發 - PostModel (#4)
    * 暫存
    
    * xxx
    
    * [Modify]改為第二週主線任務的需求
    - 新增readme.md並設定版本號，方便後續追朔過去的主線任務
    - 新增四支API路由：createPostAPI/deletePostAPI/getAllPostAPI/updatePostAPI
    - handler整理
    
    * [Modify]整理註解

--- APIResponesModule/errorHandler.js
+++ APIResponesModule/errorHandler.js
@@ -1,9 +1,10 @@
 const headers = require('./corsHeader');
-function errorHandler(res) {  
+
+function errorHandler(res, message) {  
   res.writeHead(400, headers);
   res.write(JSON.stringify({
     "status": "fail",
-    "message": "欄位未填寫正確，或無此待辦事項" 
+    "message": message,
   }));
   res.end();
 };
--- successHandler.js
+++
@@ -1,8 +0,0 @@
-const headers = require('./corsHeader');
-function successHandler(res, todoData) {
-  res.writeHead(200, headers);
-  res.write(JSON.stringify({ status: 'success', data: todoData }));
-  res.end();
-}
-
-module.exports = successHandler;
