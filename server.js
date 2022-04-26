5f12eb2b5bddf756ae0665725f624055f8b2d60f
Tofu <tsengbatty@gmail.com>
2022-04-26 09:32:09 +0800

    [Modify]更新server.js

--- server.js
+++ server.js
@@ -1,79 +1,52 @@
 const http = require('http');
-const { v4: uuidv4 } = require('uuid');
-const errorHandler = require('./errorHandler');
-const successHandler = require('./successHandler');
-const headers = require('./corsHeader');
-const todos = [];
-const requestListener = (req, res) => {
-	let body = '';
-	req.on('data', (chunk) => {
-		body += chunk;
-	});
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const getAllPostAPI = require('./Routes/getAllPostAPI'); // GET
+const createPostAPI = require('./Routes/createPostAPI'); // POST
+const deletePostAPI = require('./Routes/deletePostAPI'); // DELETE
+const updatePostAPI = require('./Routes/updatePostAPI'); // PATCH
 
-	if (req.url === '/todos' && req.method === 'GET') {
-    successHandler(res, todos);
-	} else if (req.url === '/todos' && req.method === 'POST') {
-		req.on('end', () => {
-			try {
-				const clientData = JSON.parse(body);
-				const { title } = clientData;
-				if (title !== undefined) {
-					const tempNewToDo = {
-						title: title,
-						id: uuidv4(),
-					};
-					todos.push(tempNewToDo);
-          successHandler(res, todos);
-				} else {
-					errorHandler(res);
-				}
-			} catch (error) {
-				errorHandler(res);
-			}
-		});
-	} else if (req.url === '/todos' && req.method === 'DELETE') {
-		todos.length = 0;
-    successHandler(res, todos);
-	} else if (req.url.startsWith('/todos/') && req.method === 'DELETE') {
-		const targetId = req.url.split('/').pop();
-		const targetIndex = todos.findIndex(
-			(element) => element.id === targetId
-		);
-		if (targetIndex !== -1) {
-			todos.splice(targetIndex, 1);
-			successHandler(res, todos);
-		} else {
-			errorHandler(res);
-		}
-	} else if (req.url.startsWith('/todos/') && req.method === 'PATCH') {
-		req.on('end', () => {
-			try {
-				const targetId = req.url.split('/').pop();
-				const targetIndex = todos.findIndex(
-					(element) => element.id === targetId
-				);
+dotenv.config();
+// 資料庫位置
+const dbUrl = process.env.DB_URL.replace('<password>', process.env.USER_PASSWORD).replace('<databasename>', process.env.DB_NAME);
 
-				const clientData = JSON.parse(body);
-				const { title } = clientData;
+// 連接資料庫
+mongoose.connect(dbUrl).then((response) => {
+  console.log('有連接到資料庫囉！');
+}).catch((error) => {
+  // console.log(error, 'error');
+});
 
-				if (targetIndex !== -1 && title !== undefined) {
-					todos[targetIndex].title = title;
-					successHandler(res, todos);
-				} else {
-					errorHandler(res);
-				}
-			} catch (error) {
-				errorHandler(res);
-			}
-		});
-	} else if (req.method === 'OPTIONS') {
-		res.writeHead(200, headers);
-		res.end();
-	} else {
-		res.writeHead(404, headers);
-		res.write(JSON.stringify({ status: 'fail', data: '無此網站路由' }));
-		res.end();
-	}
+const headers = {
+  'Access-Control-Allow-Headers':
+  'Content-Type, Authorization, Content-Length, X-Requested-With',
+  'Access-Control-Allow-Origin': '*',
+  'Access-Control-Allow-Methods': 'PATCH, POST, GET,OPTIONS,DELETE',
+  'Content-Type': 'application/json',
 };
+
+const requestListener = async (req, res) => {
+ 
+  if (req.url.startsWith('/posts') && req.method === 'GET') {
+    //  ASK  這邊也需要 try catch？get 也有可能會出錯
+    getAllPostAPI(req, res, headers);
+  } else if (req.url === '/posts' && req.method === 'POST') {
+    createPostAPI(req, res, headers);
+  } else if (req.url === '/posts' && req.method === 'DELETE') {
+    deletePostAPI(req, res, 'all');
+  } else if (req.url.startsWith('/posts/') && req.method === 'DELETE') {
+    deletePostAPI(req, res, 'single');
+  } else if (req.url.startsWith('/posts/') && req.method === 'PATCH') {
+    updatePostAPI(req, res);
+  } else if (req.method === 'OPTIONS') {
+    res.writeHead(200, headers);
+    res.end();
+  } else {
+    res.writeHead(404, headers);
+    res.write(JSON.stringify({ status: 'fail', data: '無此網站路由' }));
+    res.end();
+  }
+};
+
 const server = http.createServer(requestListener);
 server.listen(process.env.PORT || 3005);
