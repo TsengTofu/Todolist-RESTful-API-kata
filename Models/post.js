d1dc56e6d2b8254b2c0bbd2cc1523d94e56d7bd7
Tseng Tofu <tsengbatty@gmail.com>
2022-05-09 22:03:19 +0800

    [Modify]改變第三週主線任務改寫檔案的位置 (#9)

--- Models/post.js
+++ Models/post.js
@@ -1,25 +1,25 @@
 const mongoose = require('mongoose');
 
 const postSchema = new mongoose.Schema(
-  {
-    user_name: String,
-    user_image: String,
-    content_message: {
-      type: String,
-      required: [true, '貼文內容為必填'],
-    },
-    content_image: {
-      type: String,
-      required: [true, '貼文的圖片網址為必填'],
-    },
-    created_at: {
-      type: Date,
-      default: Date.now,
-    },
-  },
-  {
-    versionKey: false,
-  },
+	{
+		user_name: String,
+		user_image: String,
+		content_message: {
+			type: String,
+			required: [true, '貼文內容為必填'],
+		},
+		content_image: {
+			type: String,
+			required: [true, '貼文的圖片網址為必填'],
+		},
+		created_at: {
+			type: Date,
+			default: Date.now,
+		},
+	},
+	{
+		versionKey: false,
+	}
 );
 
 const Post = mongoose.model('Post', postSchema);
